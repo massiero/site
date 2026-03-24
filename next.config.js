abdb6ba525e295f0018695000302728babd6283e
massiero <massiero@gmail.com>
2026-03-24 02:49:10 -0300

    fix: revert subpath prefixes for custom domain edutic.com.br

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/site',
-    assetPrefix: '/site/',
     images: {
         unoptimized: true,
     },
